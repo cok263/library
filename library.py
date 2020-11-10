cfb568754203f99105b2a434948a4d5f8d2b19b6
cok263 <cok26385@gmail.com>
2020-11-10 20:04:59 +0300

    download books from tululu.org

--- library.py
+++ library.py
@@ -1,12 +1,16 @@
 import requests
-
+import os
 
 payload = {"id":"32168"}
 
-url = 'https://tululu.org/txt.php?id=32168'
-response = requests.get(url, verify=False)
-response.raise_for_status()
+books_dir='books'
+os.makedirs(books_dir, exist_ok=True)
+for id in range(1, 11):
+    url = 'https://tululu.org/txt.php?id={}'.format(id)
+    response = requests.get(url, verify=False, allow_redirects=False)
+    response.raise_for_status()
 
-filename = 'book.txt'
-with open(filename, 'wb') as file:
-    file.write(response.content)
+    if not response.is_redirect:
+        filename = '{}/id{}.txt'.format(books_dir, id)
+        with open(filename, 'wb') as file:
+            file.write(response.content)
