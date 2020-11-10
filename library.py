5f2c83ca6c7f3595084556fd9e0a26e09df528ac
cok263 <cok26385@gmail.com>
2020-11-10 23:18:42 +0300

    download books with titles

--- library.py
+++ library.py
@@ -1,16 +1,39 @@
-import requests
 import os
+import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
-payload = {"id":"32168"}
-
-books_dir='books'
-os.makedirs(books_dir, exist_ok=True)
-for id in range(1, 11):
-    url = 'https://tululu.org/txt.php?id={}'.format(id)
+def download_txt(url, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
     response = requests.get(url, verify=False, allow_redirects=False)
     response.raise_for_status()
 
     if not response.is_redirect:
-        filename = '{}/id{}.txt'.format(books_dir, id)
+        os.makedirs(folder, exist_ok=True)
+        filename = os.path.join(folder, sanitize_filename(filename))
         with open(filename, 'wb') as file:
             file.write(response.content)
+        return filename
+
+
+for id in range(1, 11):
+    url = 'https://tululu.org/b{}/'.format(id)
+    response = requests.get(url, verify=False, allow_redirects=False)
+    response.raise_for_status()
+    if not response.is_redirect:
+        soup = BeautifulSoup(response.text, 'lxml')
+        header = soup.find('td', class_='ow_px_td').find('h1')
+        headers = str(header).strip('</h1>').split('::')
+        title = headers[0].strip()
+        author = headers[1].split('">')[1].rstrip('</a')
+
+        url = 'https://tululu.org/txt.php?id={}'.format(id)
+        print(download_txt(url, '{}. {}.txt'.format(id, title)))
+    
