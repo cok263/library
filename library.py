84528ece666f85e96e76ca0296216d96defd5aea
cok263 <cok26385@gmail.com>
2020-11-20 00:27:46 +0300

    split long functions

--- library.py
+++ library.py
@@ -69,59 +69,71 @@ def get_books_links(start_page=1, end_page=sys.maxsize,
     return books_href
 
 
+def parse_book_page(page):
+    soup = BeautifulSoup(page, 'lxml')
+    
+    content = soup.select_one('#content')
+
+    selector_title = 'h1 > a'
+    selector_author = 'h1 a'
+    title = content.select_one(selector_title).previous_sibling
+    title = title.strip().strip(':').strip().capitalize()
+    author = content.select_one(selector_author).text.strip().title()
+
+    image_selector = '.bookimage img'
+    image_src = content.select_one(image_selector)['src']
+    url = 'https://tululu.org/txt.php?id={}'.format(id)
+    image_url = urljoin(url, image_src)
+
+    comments_selector = '.texts .black'
+    comments = [comment.get_text() for comment
+                in content.select(comments_selector)]
+
+    genres_selector = 'span.d_book a'
+    genres = [genre.get_text() for genre
+              in content.select(genres_selector)]
+
+    return {
+        'title': title,
+        'author': author,
+        'img src': image_url,
+        'comments': comments,
+        'genres': genres,
+    }
+
+
+def add_book_to_json(book_info, dest_folder, json_path):
+    json_file = os.path.join(dest_folder, json_path)
+    os.makedirs(os.path.dirname(json_file), exist_ok=True)
+    with codecs.open(json_file, "a", encoding='utf8') as books_file:
+        json.dump(book_info, books_file, ensure_ascii=False)    
+
+
+def download_book(book_url, book_page, dest_folder='',
+                  skip_imgs=False, skip_txt=False,
+                  json_path='books.json'):
+    id = book_url.split('/')[-2].lstrip('b')
+    book_info = parse_book_page(book_page)
+    add_book_to_json(book_info, dest_folder, json_path)
+    if not skip_txt:
+        download_txt(book_url, '{}. {}.txt'.format(id, book_info['title']),
+                     os.path.join(dest_folder, 'books/'))
+    if not skip_imgs:
+        download_image(book_info['img src'],
+                       book_info['img src'].split('/')[-1],
+                       os.path.join(dest_folder, 'images/'))
+
+
 def download_books(books_links, dest_folder='',
                    skip_imgs=False, skip_txt=False,
                    json_path='books.json'):
     for url in books_links:
-        print(url)
-        id = url.split('/')[-2].lstrip('b')
         response = requests.get(url, verify=False, allow_redirects=False)
         response.raise_for_status()
         if response.is_redirect:
             continue
-
-        soup = BeautifulSoup(response.text, 'lxml')
-
-        content = soup.select_one('#content')
-
-        selector_title = 'h1 > a'
-        selector_author = 'h1 a'
-        title = content.select_one(selector_title).previous_sibling
-        title = title.strip().strip(':').strip().capitalize()
-        author = content.select_one(selector_author).text.strip().title()
-
-        image_selector = '.bookimage img'
-        image_src = content.select_one(image_selector)['src']
-        url = 'https://tululu.org/txt.php?id={}'.format(id)
-        image_url = urljoin(url, image_src)
-        print(image_url)
-
-        comments_selector = '.texts .black'
-        comments = [comment.get_text() for comment
-                    in content.select(comments_selector)]
-
-        genres_selector = 'span.d_book a'
-        genres = [genre.get_text() for genre
-                  in content.select(genres_selector)]
-
-        book_dict = {
-            'title': title,
-            'author': author,
-            'img src': image_url,
-            'comments': comments,
-            'genres': genres,
-        }
-
-        json_file = os.path.join(dest_folder, json_path)
-        os.makedirs(os.path.dirname(json_file), exist_ok=True)
-        with codecs.open(json_file, "a", encoding='utf8') as books_file:
-            json.dump(book_dict, books_file, ensure_ascii=False)
-        if not skip_txt:
-            download_txt(url, '{}. {}.txt'.format(id, title),
-                         os.path.join(dest_folder, 'books/'))
-        if not skip_imgs:
-            download_image(image_url, image_url.split('/')[-1],
-                           os.path.join(dest_folder, 'images/'))
+        download_book(url, response.text, dest_folder,
+                      skip_imgs, skip_txt, json_path)
 
 
 def create_parser():
